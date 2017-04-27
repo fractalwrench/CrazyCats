c0137ec230dff493998d1e0418c37bef6a30bb2a
Jamie <fractalwrench@gmail.com>
2017-04-27 17:45:42 +0100

    setup sanity tests for jvm + instrumentation

--- app/src/androidTest/java/com/fractalwrench/crazycats/AppSmokeTest.java
+++ app/src/androidTest/java/com/fractalwrench/crazycats/AppSmokeTest.java
@@ -7,20 +7,18 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 /**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Ensures that the application builds and launches on an Android device.
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class AppSmokeTest {
+
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.fractalwrench.crazycats", appContext.getPackageName());
+        assertTrue(appContext.getPackageName().startsWith("com.fractalwrench.crazycats"));
     }
+
 }
---
+++ app/src/test/java/com/fractalwrench/crazycats/SanityTest.java
@@ -0,0 +1,17 @@
+package com.fractalwrench.crazycats;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Sanity test to ensure that JUnit is setup correctly
+ */
+public class SanityTest {
+
+    @Test
+    public void checkAddition() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+}
